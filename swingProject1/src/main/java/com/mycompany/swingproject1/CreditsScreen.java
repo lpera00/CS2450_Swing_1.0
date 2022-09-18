f652708b9f114d927e044127b7dccb1a22479749
pkflunr <71987718+pkflunr@users.noreply.github.com>
2022-09-18 14:44:38 -0700

    Added actual credits to credits screen

--- swingProject1/src/main/java/com/mycompany/swingproject1/CreditsScreen.java
+++ swingProject1/src/main/java/com/mycompany/swingproject1/CreditsScreen.java
@@ -4,39 +4,44 @@
  */
 package com.mycompany.swingproject1;
 
-import java.awt.Button;
-import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 /**
  *
  * @author PK_Flunr
  */
 class CreditsScreen extends JPanel {
-    private Button backButton;
+    private JButton backButton;
+    
+    private JLabel creditsLabel;
 
     public CreditsScreen(){
         setSize(600,400);
         setVisible(true);
+        
+        // label holding credits
+        var creditsText = "<html>Credits:<br/>Miguel Valmonte 014714427<br/>Lindsey Pera 015215889</html>";
+        creditsLabel = new JLabel(creditsText);
+        Dimension creditsDimension = creditsLabel.getPreferredSize();
+        creditsLabel.setBounds(250,175,creditsDimension.width,creditsDimension.height);
+        add(creditsLabel);
+        
         //back button
-            backButton = new Button("back");
+            backButton = new JButton("back");
             backButton.setBounds(475, 325, 100, 20);
             backButton.addActionListener(new ActionListener(){
                 public void actionPerformed(ActionEvent e){
                     backButton.setEnabled(false);
                     backButton.setVisible(false);
                     repaint();
+                    removeAll();
                     setEnabled(false);
                     add(new MenuScreen());
                 }
             });
             add(backButton);
-    }
-    
-    public void paintComponent(Graphics g){
-        g.drawString("credits screen", 250, 175);
     }
 
 }
